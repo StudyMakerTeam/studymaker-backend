314c5d15fdbf06b650c7ec494e631ba80d833968
Ray <55176622+kdl9405@users.noreply.github.com>
2021-04-11 18:10:09 +0900

    스프링 링크 - 리엑트 페이지 매핑 해야함

--- src/main/java/com/anytime/studymaker/Controller/MainController.java
+++ src/main/java/com/anytime/studymaker/Controller/MainController.java
@@ -1,11 +1,21 @@
 package com.anytime.studymaker.Controller;
 
+import java.util.Date;
+
 import com.anytime.studymaker.Controller.common.CrudController;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController implements CrudController {
 
-//    TODO : 메인
+    @GetMapping("/main")
+    public String hello() {
+        return "안녕하세요. 현재 서버시간은 " + new Date() + "입니다. \n";
+
+    }
+
+    // TODO : 메인
 
 }
