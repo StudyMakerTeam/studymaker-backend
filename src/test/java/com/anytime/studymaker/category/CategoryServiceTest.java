f21ef17a6dd5f0cd1a903c2d316c8ca70efbf9d5
jeongyong <jeongyong95@gmail.com>
2021-09-15 23:14:01 +0900

    aws codepipeline용 buildspec 작성

--- src/test/java/com/anytime/studymaker/category/CategoryServiceTest.java
+++ src/test/java/com/anytime/studymaker/category/CategoryServiceTest.java
@@ -1,29 +1,29 @@
-package com.anytime.studymaker.category;
-
-import com.anytime.studymaker.controller.dto.CategoryCreateRequest;
-import com.anytime.studymaker.domain.category.Category;
-import com.anytime.studymaker.domain.category.CategoryService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class CategoryServiceTest {
-
-    @Autowired
-    private CategoryService categoryService;
-
-    @Test
-    public void create() {
-        CategoryCreateRequest request = CategoryCreateRequest.builder().categoryName("Language").build();
-
-        Category response = categoryService.create(request);
-        Assertions.assertNotNull(response);
-    }
-
-    @Test
-    public void read() {
-
-    }
-}
+//package com.anytime.studymaker.category;
+//
+//import com.anytime.studymaker.controller.dto.CategoryCreateRequest;
+//import com.anytime.studymaker.domain.category.Category;
+//import com.anytime.studymaker.domain.category.CategoryService;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class CategoryServiceTest {
+//
+//    @Autowired
+//    private CategoryService categoryService;
+//
+//    @Test
+//    public void create() {
+//        CategoryCreateRequest request = CategoryCreateRequest.builder().categoryName("Language").build();
+//
+//        Category response = categoryService.create(request);
+//        Assertions.assertNotNull(response);
+//    }
+//
+//    @Test
+//    public void read() {
+//
+//    }
+//}
